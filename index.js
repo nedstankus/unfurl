4b2aa42039525ba832568fdd33d237104d26e555
Ned Stankus <nstankus@gmail.com>
2018-12-12 07:12:09 -0700

    Code cleanup

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var unfurl = require('unfurled');
-const util = require('util')
-
+const util = require('util');
 
 let targetUrl = process.argv[2];
 
-console.log("Scanning " + process.argv[2]);
-
 if (!targetUrl) return;
+
+//TODO add validation for targetUrl
+
+console.log("Scanning " + targetUrl);
 
 ;(async function () {
 
-  let result = await unfurl(targetUrl, {"ogp":true,"oembed":true,"twitter":true,"other":true})
+	let result = await unfurl(targetUrl, {"ogp":true,"oembed":true,"twitter":true,"other":true});
 
-  console.log('result', util.inspect(result, {showHidden: false, depth: null}))
+	console.log('result', util.inspect(result, {showHidden: false, depth: null}));
 
 })().catch(console.error)
